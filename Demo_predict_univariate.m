2eee675d6e76fb92333d1ede99e536bd8dd78a72
LMNonlinear <min.li.231314@gmail.com>
2021-01-08 23:25:38 +0800

    Gau is better , and the select number is need robust

--- Demo_predict_univariate.m
+++ Demo_predict_univariate.m
@@ -1,7 +1,7 @@
 % Demo
 clear;clc;close all
 
-order=2;  kernel_name='GAU';
+order=1;  kernel_name='GAU';
 fun = @(X) (30+(5*X+5).*sin(5*X+5)) .* (4+exp(-(2.5*X+2.5).^2));
 X= linspace(-3,3,2/0.01)';
 rng(1);
@@ -12,23 +12,22 @@ params = lwpparams(kernel_name, order, false, hBest); %% outer need h select bas
 Xq =linspace(-3,3,2/0.005)';
 [Yq,Lq] = lwppredict(X, Y, params,Xq);
 figure;
-plot(Xq,Yq);
+plot(X,Y);
 hold on;
-plot(Xq, fun(Xq), 'r.', 'Markersize', 20);
+plot(Xq, fun(Xq), 'r.', 'Markersize', 10);
 MSE = lwpeval(X, Y, params, 'VD', Xq, fun(Xq));
 %% predict
-% params = lwpparams(kernel_name, order, false); %% knn true is not for GAU GAR
-% [hBest, critBest, results] = lwpfindh(Xq, Yq, params, 'CV');
+params = lwpparams(kernel_name, order, false); %% knn true is not for GAU GAR
+[hBest, critBest, results] = lwpfindh(Xq, Yq, params, 'CV');
 params = lwpparams(kernel_name, order, false, []);
 params.outer=1;
 % params.safe=false;
-
-Xp=linspace(-5,5,2/0.01)';
-[Yp] = lwppredict(X, Y, params,Xp);
+Xp=linspace(-4.5,4.5,2/0.01)';
+[Yp,Lq] = lwppredict(X, Y, params,Xp);
 figure;
 plot(Xp,Yp);
 hold on;
-plot(Xp,fun(Xp), 'r.', 'Markersize', 20);
+plot(Xp,fun(Xp), 'r.', 'Markersize', 5);
 MSE = lwpeval(X, Y, params, 'VD', Xp, fun(Xp));
 
 
